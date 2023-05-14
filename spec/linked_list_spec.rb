92765f079efd043b53c9084a5f0326fa2af4b97a
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-13 17:11:12 -0800

    add comments to LinkedList tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -5,14 +5,17 @@ RSpec.describe do
   before(:each) do
     @list = LinkedList.new
   end
+  # Places the given code at the beginning of eact test.
 
   it 'is a linked list' do
     expect(@list).to be_a(LinkedList)
   end
+  # New lists, even if empty are LinkedLists.
   
   it 'has a nil head' do
     expect(@list.head).to be(nil)
   end
+  # New lists without nodes, have a head equal to nil.
 
   it 'can append a node' do
     @list.append('doop')
@@ -20,6 +23,7 @@ RSpec.describe do
     expect(@list.head.data).to eq("doop")
     expect(@list.head.next).to eq(nil) 
   end
+  # Empty Lists can be given a head node with append method.
 
   it 'has multiple nodes' do
     @list.append('doop')
@@ -28,16 +32,19 @@ RSpec.describe do
     expect(@list.head.data).to eq("doop")
     expect(@list.head.next.data).to eq("deep")
   end
+  # Append will continue to add new nodes to the end of the list.
 
   it 'can count the number of nodes' do
     @list.append('doop')
     @list.append('deep')
     expect(@list.count).to eq(2)
   end
+  # Count will return the number of nodes in the list.
 
   it 'can print all list data' do
     @list.append('doop')
     @list.append('deep')
     expect(@list.to_string).to eq("doop deep")
   end
+  # to_string will return the entire list as a string.
 end
