b70044793c423be225b1a931ce277ab5f594999f
Chris Cullinane <chriscullinane@protonmail.com>
2023-05-14 00:23:39 -0800

    add append method and test for BeatBox

--- lib/beat_box.rb
+++ lib/beat_box.rb
@@ -4,5 +4,14 @@ class BeatBox
   def initialize
     @list = LinkedList.new
   end
+  # Adds an empty list to the new BeatBox.
+
+  def append(data)
+    new_beats = data.split(" ")
+    new_beats.each do |beat| @list.append(beat)
+    end
+    new_beats
+  end
+  # Splits new beats into nodes and appends them to the list.
 
 end
--- spec/beat_box_spec.rb
+++ spec/beat_box_spec.rb
@@ -9,5 +9,17 @@ RSpec.describe BeatBox do
 
   it 'is a BeatBox' do
     expect(@bb).to be_a(BeatBox)
+    expect(@bb.list).to be_a(LinkedList)
+    expect(@bb.list.head).to eq(nil)
   end
+  # new works as expected
+
+  it 'can append many beats in a string' do
+    @bb.append('deep doo ditt')
+    expect(@bb.list.head.data).to eq('deep')
+    expect(@bb.list.head.next.data).to eq('doo')
+    @bb.append('woo hoo shu')
+    expect(@bb.list.tail.data).to eq('shu')
+  end
+  # appends new beats as new nodes in list.
 end
